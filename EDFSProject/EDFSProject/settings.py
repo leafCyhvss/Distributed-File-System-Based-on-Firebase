76336f5599bd8291058afa8ea23148c98f4d03bd
Cypher <46045772+leafCyhvss@users.noreply.github.com>
2022-11-26 14:26:41 -0800

    mysql

--- EDFSProject/EDFSProject/settings.py
+++ EDFSProject/EDFSProject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-from mysql.connector import django
+# from mysql.connector import django
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
