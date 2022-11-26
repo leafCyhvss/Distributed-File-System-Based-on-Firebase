8ee2fd08cdc45fdfe8d83b71b608acb0dc1a0d0a
Carra Hamner <carrah@usc.edu>
2022-11-26 14:25:07 -0800

    updated html templates

--- EDFSProject/EDFSProject/settings.py
+++ EDFSProject/EDFSProject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from mysql.connector import django
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
