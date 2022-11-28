089dd57af52a57584d9a41b11e43b633cddffdfa
Cypher <46045772+leafCyhvss@users.noreply.github.com>
2022-11-28 13:40:59 -0800

    Update mapreduce.py

--- EDFSProject/MapReduce/mapreduce.py
+++ EDFSProject/MapReduce/mapreduce.py
@@ -1,17 +1,21 @@
+import pandas as pd
+
 from EDFS2.EDFS2 import EDFSURL
 from EDFS1 import edfs
+
 
 class MapReduce():
 
     def __init__(self):
         self.edfs2 = EDFSURL()
+
     def searchMapper(self, filepaths, edfsType):
         # [{toyota.csv: df1},{toyota.csv: df2},{audi.csv: df3}]
         # 根据文件路径，获取分区数量
         # 大for循环
-            # 对每个分区的文件分别下载
-            # 对每个分区的处理
-            # 结果[filename, value]保存到结果ans中
+        # 对每个分区的文件分别下载
+        # 对每个分区的处理
+        # 结果[filename, value]保存到结果ans中
         ans = []
         if edfsType == '2':
             for filepath in filepaths:
@@ -23,16 +27,14 @@ class MapReduce():
                         dataset = self.edfs2.getFilebyDatanode(datanode)
                         # 找出某个条件的数据行
                         # 数据行 存df
-                        searchedDf = dataset.loc[(dataset['price']>12000)\
-                                                 &(dataset['transmission'] == 'Manual')]
+                        searchedDf = dataset.loc[(dataset['price'] > 12000) \
+                                                 & (dataset['transmission'] == 'Manual')]
                         if len(searchedDf.index) != 0:
                             ans.append({fileName: searchedDf})
             print('ans\n', ans)
             return ans
         if edfsType == '1':
             pass
-
-
 
     def searchReducer(self, keyValuePair):
         '''
@@ -43,13 +45,16 @@ class MapReduce():
         :return:
         '''
         import collections
-        dataset = collections.defaultdict(lambda: [])
+        tmpdataset = collections.defaultdict(lambda: [])
+        finalData = collections.defaultdict(lambda: pd.DataFrame())
         for dic in keyValuePair:
             fileName = list(dic.keys())[0]
-            dataset[fileName].append(list(dic.values())[0])
-        for key, value in dataset.items():
-            for partData in value:
-
+            tmpdataset[fileName].append(list(dic.values())[0])
+        for fileName, DFlist in tmpdataset.items():
+            for partData in DFlist:
+                finalData[fileName] = pd.concat([finalData[fileName], partData], ignore_index=True)
+        print('='*50)
+        print(finalData)
 
 
 x = MapReduce()
